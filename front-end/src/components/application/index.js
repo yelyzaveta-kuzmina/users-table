de41c85fb9957f67075d54c08dc4b8e9c2ebbbd3
Yelyzaveta <elizaveta.kzm@gmail.com>
2019-10-11 11:13:48 +0200

    first commit

---
+++ front-end/src/components/application/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import styles from './styles.module.scss';
+
+class Application extends React.Component {
+  render() {
+    return <div>123</div>;
+  }
+}
+
+export default Application;
