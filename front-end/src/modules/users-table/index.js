a3ab4cb70678642bc6984a5f6382577cf1c16d30
Yelyzaveta <elizaveta.kzm@gmail.com>
2019-10-18 17:45:36 +0300

    styled sorting arrows

--- front-end/src/modules/users-table/index.js
+++ front-end/src/modules/users-table/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusCode, toggleSortingDirection } from '../../utils.js';
 import Header from '../header';
 import UsersList from '../../components/user-list';
+import classNames from 'classnames';
 import styles from './styles.module.scss';
 
 const API_ORIGIN = process.env.API_ORIGIN || 'http://localhost:8080';
@@ -49,14 +50,19 @@ class UsersTable extends React.Component {
   };
 
   render() {
-    const { users } = this.state;
+    const { users, sortingDirection, sortBy } = this.state;
+    console.log(sortingDirection);
 
     return (
       <div className={styles.wrapper}>
         <Header />
         <span className={styles.tableHeader}>
           <div className={styles.nameHeader}>
-            <button className={styles.directionArrow} onClick={() => this.onSortBsChange('name')}>
+            <button
+              className={classNames(styles.ascArrowDirection, {
+                [styles.descArrowDirection]: sortingDirection === 'desc' && sortBy === 'name'
+              })}
+              onClick={() => this.onSortByChange('name')}>
               &darr;
             </button>
             Name
@@ -65,7 +71,11 @@ class UsersTable extends React.Component {
           <div className={styles.countryHeader}>Country</div>
           <div className={styles.genderHeader}>Gender</div>
           <div className={styles.ageHeader}>
-            <button className={styles.directionArrow} onClick={() => this.onSortByChange('age')}>
+            <button
+              className={classNames(styles.ascArrowDirection, {
+                [styles.descArrowDirection]: sortingDirection === 'desc' && sortBy === 'age'
+              })}
+              onClick={() => this.onSortByChange('age')}>
               &darr;
             </button>
             Age
